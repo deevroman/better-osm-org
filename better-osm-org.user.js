5023d21f9a015c3e64c79231211b82327538db8a
Roman Deev <roman.deev06@gmail.com>
2023-10-29 23:52:22 +0300

    Update better-osm-org.user.js

--- better-osm-org.user.js
+++ better-osm-org.user.js
@@ -3,7 +3,7 @@
 // @version      0.1
 // @description  Several improvements for advanced users of osm.org
 // @author       deevroman
-// @match        https://www.openstreetmap.org/changeset/*
+// @match        https://www.openstreetmap.org/*
 // @license      WTFPL
 // @namespace    https://github.com/deevroman/better-osm-org
 // @updateURL    https://github.com/deevroman/better-osm-org/raw/master/better-osm-org.user.js
