61cce3166d346e098279a553a141abbd3f671fab
Roman Deev <roman.deev06@gmail.com>
2023-11-28 21:14:39 +0300

    Create finish-oauth.js

---
+++ finish-oauth.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+window.addEventListener('load', function(e) {
+    opener.authComplete(window.location.href);
+    window.close();
+});
+</script>
+</head>
+<body>Wait...</body>
+</html>
