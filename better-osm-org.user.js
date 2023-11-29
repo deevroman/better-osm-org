2c04924b5ce41237aac109f4f1364186351359fb
Roman Deev <roman.deev06@gmail.com>
2023-11-29 04:15:48 +0300

    Delete button

--- better-osm-org.user.js
+++ better-osm-org.user.js
@@ -13,58 +13,91 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=openstreetmap.org
 // @grant        GM_registerMenuCommand
 // @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
+// @require      https://raw.githubusercontent.com/Zverik/osmtags-editor/main/osm-auth.iife.min.js
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM.getValue
 // @grant        GM.setValue
+// @grant        GM_getResourceURL
+// @resource     OAUTH_HTML https://github.com/deevroman/better-osm-org/raw/master/finish-oauth.html
 // ==/UserScript==
 GM_config.init(
-{
-    'id': 'Config',
-    'title': 'Better osm.org settings',
-    'fields':
     {
-        'RevertButton':
+        'id': 'Config',
+        'title': 'Better osm.org settings',
+        'fields':
         {
-            'label': 'Revert button',
-            'type': 'checkbox',
-            'default': 'checked'
+            'RevertButton':
+            {
+                'label': 'Revert button',
+                'type': 'checkbox',
+                'default': 'checked'
+            },
+            'MassRevert':
+            {
+                'label': 'Mass revert selectors',
+                'type': 'checkbox',
+                'default': 'checked'
+            },
+            'CompactChangesetsHistory':
+            {
+                'label': 'Compact changesets history',
+                'type': 'checkbox',
+                'default': 'checked'
+            },
+            'ResolveNotesButtons':
+            {
+                'label': 'Show addition resolve buttons',
+                'type': 'checkbox',
+                'default': 'unchecked'
+            },
+            'Deletor':
+            {
+                'label': 'Show buttons for object deletion',
+                'type': 'checkbox',
+                'default': 'checked'
+            },
         },
-        'MassRevert':
-        {
-            'label': 'Mass revert selectors',
-            'type': 'checkbox',
-            'default': 'checked'
-        },
-        'CompactChangesetsHistory':
-        {
-            'label': 'Compact changesets history',
-            'type': 'checkbox',
-            'default': 'checked'
-        },
-        'ResolveNotesButtons':
-        {
-            'label': 'Show addition resolve buttons',
-            'type': 'checkbox',
-            'default': 'checked'
-        },
-    },
-    frameStyle: [
-      'bottom: auto; border: 1px solid #000; display: none; height: 75%;',
-      'left: 0; margin: 0; max-height: 95%; max-width: 95%; opacity: 0;',
-      'overflow: auto; padding: 0; position: fixed; right: auto; top: 0;',
-      'width: 20%; z-index: 9999;'
-	].join(' '),
-});
+        frameStyle: [
+            'bottom: auto; border: 1px solid #000; display: none; height: 75%;',
+            'left: 0; margin: 0; max-height: 95%; max-width: 95%; opacity: 0;',
+            'overflow: auto; padding: 0; position: fixed; right: auto; top: 0;',
+            'width: 20%; z-index: 9999;'
+        ].join(' '),
+    });
 
 let onInit = config => new Promise(resolve => {
-  let isInit = () => setTimeout(() =>
-    config.isInit ? resolve() : isInit(), 0);
-  isInit();
+    let isInit = () => setTimeout(() =>
+                                  config.isInit ? resolve() : isInit(), 0);
+    isInit();
 });
 
 let init = onInit(GM_config);
 
+let apiBase = "https://master.apis.dev.openstreetmap.org/api/0.6/"; // TODO
+
+function tagsToXml(doc, node, tags) {
+    for (const [k, v] of Object.entries(tags)) {
+        let tag = doc.createElement('tag');
+        tag.setAttribute('k', k);
+        tag.setAttribute('v', v);
+        node.appendChild(tag);
+    }
+}
+
+function makeAuth() {
+    return osmAuth.osmAuth({
+        apiUrl: "https://master.apis.dev.openstreetmap.org/api/0.6",
+        url: "https://master.apis.dev.openstreetmap.org",
+        // Put your own credentials here.
+        client_id: "FwA",
+        client_secret: "ZUq",
+        // Hopefully this page is never used.
+        redirect_uri: GM_getResourceURL("OAUTH_HTML"),
+        scope: "write_api",
+        auto: true
+    });
+}
 
 function addRevertButton(){
     if (document.querySelector('.revert_button_class')) return true;
@@ -169,6 +202,106 @@ function setupResolveNotesButtons() {
     }
 }
 
+function addDeleteButton() {
+    if (document.querySelector('.delete_object_button_class')) return true;
+
+    let match = location.href.match(/(node|way)\/(\d+)/);
+    if (!match) return;
+    let object_type = match[1];
+    let object_id = match[2];
+
+    const auth = makeAuth();
+    let link = document.createElement('a');
+    link.text = "Выпилить!";
+    link.href = "";
+    link.classList.add("delete_object_button_class");
+    if(document.querySelector(".browse-section .latitude") === null){
+        link.setAttribute("hidden", true);
+        return;
+    }
+
+    if (!document.querySelector(".secondary-actions")) return;
+    document.querySelector(".secondary-actions").appendChild(link);
+    link.onclick = (e) => {
+        e.preventDefault();
+        const changesetTags = {
+            'created_by': 'better osm.org',
+            'comment': 'Delete ' + object_type + " " + object_id
+        };
+        let changesetPayload = document.implementation.createDocument(null, 'osm');
+        let cs = changesetPayload.createElement('changeset');
+        changesetPayload.documentElement.appendChild(cs);
+        tagsToXml(changesetPayload, cs, changesetTags);
+        const chPayloadStr = new XMLSerializer().serializeToString(changesetPayload);
+
+        console.log("Opening changeset");
+
+        auth.xhr({
+            method: 'GET',
+            path: apiBase + object_type + '/' + object_id,
+            prefix: false,
+        }, function(err, objectInfo) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            auth.xhr({
+                method: 'PUT',
+                path: apiBase + 'changeset/create',
+                prefix: false,
+                content: chPayloadStr
+            }, function(err1, result) {
+                const changesetId = result;
+                console.log(changesetId);
+                objectInfo.children[0].children[0].setAttribute('changeset', changesetId);
+                auth.xhr({
+                    method: 'DELETE',
+                    path: apiBase + object_type +'/' + object_id,
+                    prefix: false,
+                    content: objectInfo
+                }, function(err2, result) {
+                    if (err2) {
+                        console.log({changesetError: err2});
+                    }
+                    auth.xhr({
+                        method: 'PUT',
+                        path: apiBase + 'changeset/' + changesetId + '/close',
+                        prefix: false
+                    }, function(err3, result) {
+                        if (!err3) {
+                            window.location.reload();
+                        }
+                    });
+                });
+            });
+        });
+    };
+}
+
+function setupDeletor(){
+    if (!GM_config.get('Deletor')) {
+        return;
+    }
+    if (!location.href.includes("/node/") && !location.href.includes("/way/")){
+        return;
+    }
+    let lastUrl = location.href;
+    new MutationObserver(() => {
+        const url = location.href;
+        if (url !== lastUrl) {
+            lastUrl = url;
+            if (!url.includes("/node/") && !url.includes("/way/")) return;
+            let timerId = setInterval(() => {
+                addDeleteButton();
+            }, 100);
+            setTimeout(() => { clearInterval(timerId); console.log('stop try add delete button'); }, 3000);
+        }
+    }).observe(document, {subtree: true, childList: true});
+    if (location.href.includes("/node/") || url.includes("/way/")){
+        addDeleteButton();
+    }
+}
+
 function setup() {
     try {
         setupRevertButton();
@@ -183,6 +316,11 @@ function setup() {
     try {
         setupResolveNotesButtons();
     } catch {
+
+    }
+    try {
+        setupDeletor();
+    } finally {
 
     }
 }
