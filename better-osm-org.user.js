8ee457cbb20ad8308a6f0bafe0714fb365eb1aec
Roman Deev <roman.deev06@gmail.com>
2024-12-18 01:53:58 +0300

    update dark theme for iD, try fix white cell from OSM wikidata extenstion, add winter Easter egg in setting menu, tags counter for object version page

--- better-osm-org.user.js
+++ better-osm-org.user.js
@@ -59,7 +59,7 @@
 // @resource     RELATION_ICON https://github.com/deevroman/better-osm-org/raw/master/icons/Taginfo_element_relation.svg
 // @resource     OSMCHA_LIKE https://github.com/OSMCha/osmcha-frontend/raw/94f091d01ce5ea2f42eb41e70cdb9f3b2d67db88/src/assets/thumbs-up.svg
 // @resource     OSMCHA_DISLIKE https://github.com/OSMCha/osmcha-frontend/raw/94f091d01ce5ea2f42eb41e70cdb9f3b2d67db88/src/assets/thumbs-down.svg
-// @resource     DARK_THEME_FOR_ID_CSS https://gist.githubusercontent.com/deevroman/55f35da68ab1efb57b7ba4636bdf013d/raw/72e47205c4fa9147e9e6211287f60be4fa1370c2/dark.css
+// @resource     DARK_THEME_FOR_ID_CSS https://gist.githubusercontent.com/deevroman/55f35da68ab1efb57b7ba4636bdf013d/raw/55babb3017ef54370f3596750a5ed999d0836233/dark.css
 // @run-at       document-end
 // ==/UserScript==
 //<editor-fold desc="config" defaultstate="collapsed">
@@ -1652,6 +1652,8 @@ function makeLinksInTagsClickable() {
             }
         }
     })
+    const tagsTable = document.querySelector(".browse-tag-list")
+    tagsTable.parentElement.previousElementSibling.title = tagsTable.querySelectorAll("tr th").length + " tags"
 }
 
 function addHistoryLink() {
@@ -1681,6 +1683,14 @@ function addHistoryLink() {
     }
     makeLinksInTagsClickable()
     makeHashtagsClickable()
+    setTimeout(() => {
+        GM_addElement(document.head, "style", {
+            textContent: `
+            table.browse-tag-list tr td[colspan="2"]{
+                background: var(--bs-body-bg) !important;
+            }`,
+        }, 0);
+    })
 }
 
 
@@ -3582,6 +3592,10 @@ function addDiffInHistory() {
             background: black;
         }
     }
+    
+    table.browse-tag-list tr td[colspan="2"] {
+        background: var(--bs-body-bg) !important;
+    }
     ` + (GM_config.get("ShowChangesetGeometry") ? `
     .way-version-view:hover {
         background-color: yellow;
@@ -5050,6 +5064,11 @@ function addQuickLookStyles() {
               position: absolute;
               color: var(--bs-body-color);
             }
+            
+            table.browse-tag-list tr td[colspan="2"]{
+                background: var(--bs-body-bg) !important;
+            }
+            
             `
             + ((GM_config.get("ShowChangesetGeometry")) ? `
             #sidebar_content #changeset_nodes li:hover {
@@ -7589,6 +7608,96 @@ function main() {
             if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
                 GM_registerMenuCommand("Check script updates", function () {
                     window.open("https://raw.githubusercontent.com/deevroman/better-osm-org/master/better-osm-org.user.js", "_blank")
+                });
+            }
+            // New Year Easter egg
+            const curDate = new Date()
+            if (curDate.getMonth() === 11 && curDate.getDate() > 18 || curDate.getMonth() === 0 && curDate.getDate() < 14) {
+                GM_registerMenuCommand("☃️", function () {
+                    //<editor-fold desc="config" defaultstate="collapsed">
+                    injectJSIntoPage(`
+                    // This code distibuted under MIT license
+                    // Author: https://github.com/DevBubba/Bookmarklets
+                    // Code was deminified
+                    function snow(t) {
+                        function i() {
+                            this.D = function () {
+                                const t = h.atan(this.i / this.d);
+                                l.save(), l.translate(this.b, this.a), l.rotate(-t), l.scale(this.e, this.e * h.max(1, h.pow(this.j, .7) / 15)), l.drawImage(m, -v / 2, -v / 2), l.restore()
+                            }
+                        }
+
+                        window;
+                        const h = Math, r = h.random, a = document, o = Date.now;
+                        e = (t => {
+                            l.clearRect(0, 0, _, f), l.fill(), requestAnimationFrame(e);
+                            const i = .001 * y.et;
+                            y.r();
+                            const s = L.et * g;
+                            for (var n = 0; n < C.length; ++n) {
+                                const t = C[n];
+                                t.i = h.sin(s + t.g) * t.h, t.j = h.sqrt(t.i * t.i + t.f), t.a += t.d * i, t.b += t.i * i, t.a > w && (t.a = -u), t.b > b && (t.b = -u), t.b < -u && (t.b = b), t.D()
+                            }
+                        }), s = (t => {
+                            for (var e = 0; e < p; ++e) C[e].a = r() * (f + u), C[e].b = r() * _
+                        }), n = (t => {
+                            c.width = _ = innerWidth, c.height = f = innerHeight, w = f + u, b = _ + u, s()
+                        });
+
+                        class d {
+                            constructor(t, e = !0) {
+                                this._ts = o(), this._p = !0, this._pa = o(), this.d = t, e && this.s()
+                            }
+
+                            get et() {
+                                return this.ip ? this._pa - this._ts : o() - this._ts
+                            }
+
+                            get rt() {
+                                return h.max(0, this.d - this.et)
+                            }
+
+                            get ip() {
+                                return this._p
+                            }
+
+                            get ic() {
+                                return this.et >= this.d
+                            }
+
+                            s() {
+                                return this._ts = o() - this.et, this._p = !1, this
+                            }
+
+                            r() {
+                                return this._pa = this._ts = o(), this
+                            }
+
+                            p() {
+                                return this._p = !0, this._pa = o(), this
+                            }
+
+                            st() {
+                                return this._p = !0, this
+                            }
+                        }
+
+                        const c = a.createElement("canvas");
+                        H = c.style, H.position = "fixed", H.left = 0, H.top = 0, H.width = "100vw", H.height = "100vh", H.zIndex = "100000", H.pointerEvents = "none", a.body.insertBefore(c, a.body.children[0]);
+                        const l = c.getContext("2d"), p = 300, g = 5e-4, u = 20;
+                        let _ = c.width = innerWidth, f = c.height = innerHeight, w = f + u, b = _ + u;
+                        const v = 15.2, m = a.createElement("canvas"), E = m.getContext("2d"),
+                            x = E.createRadialGradient(7.6, 7.6, 0, 7.6, 7.6, 7.6);
+                        x.addColorStop(0, "hsla(255,255%,255%,1)"), x.addColorStop(1, "hsla(255,255%,255%,0)"), E.fillStyle = x, E.fillRect(0, 0, v, v);
+                        let y = new d(0, !0), C = [], L = new d(0, !0);
+                        for (var j = 0; j < p; ++j) {
+                            const t = new i;
+                            t.a = r() * (f + u), t.b = r() * _, t.c = 1 * (3 * r() + .8), t.d = .1 * h.pow(t.c, 2.5) * 50 * (2 * r() + 1), t.d = t.d < 65 ? 65 : t.d, t.e = t.c / 7.6, t.f = t.d * t.d, t.g = r() * h.PI / 1.3, t.h = 15 * t.c, t.i = 0, t.j = 0, C.push(t)
+                        }
+                        s(), EL = a.addEventListener, EL("visibilitychange", () => setTimeout(n, 100), !1), EL("resize", n, !1), e()
+                    };snow();`)
+                    //</editor-fold>
+
                 });
             }
             // GM_registerMenuCommand("Ask question on forum", function () {
