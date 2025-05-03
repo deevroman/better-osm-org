71a76f0afb6bc2d4dc7c91169a6b2b182b576aee
Roman Deev <roman.deev06@gmail.com>
2025-05-03 15:09:53 +0300

    bump version

--- better-osm-org.user.js
+++ better-osm-org.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Better osm.org
 // @name:ru         Better osm.org
-// @version         0.9.9.1
+// @version         0.9.9.2
 // @changelog       v0.9.9: Button for 3D view building in OSMBuilding, F4map and other viewers
 // @changelog       v0.9.9: Key1 for open first user's changeset, add poweruser=true in Rapid link
 // @changelog       v0.9.9: Restore navigation links on changeset page of deleted user
