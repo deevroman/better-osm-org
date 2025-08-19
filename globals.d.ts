6cd085fc6e11728bff6d9b96fc602edfc3bd9edc
Roman Deev <roman.deev06@gmail.com>
2025-08-19 21:26:48 +0300

    Prettier for globals.d.ts

--- globals.d.ts
+++ globals.d.ts
@@ -12,12 +12,25 @@ declare function cloneInto<T>(
     obj: T,
     targetScope: object,
     options?: {
-        cloneFunctions?: boolean;
-        wrapReflectors?: boolean;
-    }
-): T;
         cloneFunctions?: boolean
         wrapReflectors?: boolean
+    },
 ): T
 
 import "typed-query-selector/strict"
+
+declare const osmAuth
+declare const GM
+declare const GM_info
+declare const GM_config
+declare const GM_addElement
+declare const GM_listValues
+declare const GM_deleteValue
+declare const GM_getResourceURL
+declare const GM_getResourceText
+declare const GM_registerMenuCommand
+declare const unsafeWindow
+declare const EXIF
+declare const osmtogeojson
+declare const opening_hours
+declare const runSnowAnimation
