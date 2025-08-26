03d5e8845904d465c18315e0e74e4e27f0616564
Roman Deev <roman.deev06@gmail.com>
2025-08-27 02:55:59 +0300

    bugfix for #422

--- better-osm-org.user.js
+++ better-osm-org.user.js
@@ -14648,22 +14648,22 @@ if (isOsmServer()) {
                             }
                         }
                         if (window.notesQFilter && window.invertQ) {
-                            const words = window.notesQFilter.split(",").map(i => i.trim())
+                            const words = window.notesQFilter.split(",").map(i => i.trim()).filter(i => i !== "")
                             for (const word of words) {
-                                if (word !== "" && note.properties.comments?.[0]?.text.includes(word)) {
+                                if (note.properties.comments?.[0]?.text.includes(word)) {
                                     return false
                                 }
                             }
                         }
                         if (!window.invertQ /*&& window.notesQFilter.includes(",")*/) {
-                            const words = window.notesQFilter.split(",").map(i => i.trim())
+                            const words = window.notesQFilter.split(",").map(i => i.trim()).filter(i => i !== "")
                             let found = false
                             for (const word of words) {
-                                if (word !== "" && note.properties.comments?.[0]?.text.includes(word)) {
+                                if (note.properties.comments?.[0]?.text.includes(word)) {
                                     found = true
                                 }
                             }
-                            if (!found) {
+                            if (!found && words.length) {
                                 return false
                             }
                         }
