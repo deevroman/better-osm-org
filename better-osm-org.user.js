8bf3593406545d075131ebd164c6da9608901a4f
Roman Deev <roman.deev06@gmail.com>
2025-09-29 00:39:19 +0300

    format

--- better-osm-org.user.js
+++ better-osm-org.user.js
@@ -9922,7 +9922,9 @@ function makeContextMenuElem(e) {
 
 // prettier-ignore
 const copyBtnSvg =
-    '<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-icon lucide-file">' +
+    '<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" ' +
+    'stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" ' +
+    'class="lucide lucide-file-icon lucide-file">' +
     '<path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"/>' +
     '<path d="M14 2v4a2 2 0 0 0 2 2h4"/>' +
     '</svg>'
