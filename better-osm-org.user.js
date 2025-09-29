66f6694a99f686c00e5265f7d54747b8b9ddacfc
Roman Deev <roman.deev06@gmail.com>
2025-09-30 02:14:11 +0300

    format

--- better-osm-org.user.js
+++ better-osm-org.user.js
@@ -15850,10 +15850,12 @@ function addMassActionForGlobalChangesets() {
 const CORPORATE_EMOJI = "🏢"
 const BAN_EMOJI = "⛔️"
 
+// prettier-ignore
 const moderatorBadgeSvg =
     '<svg width="20" height="20">' +
     '<path d="M 10,2 8.125,8 2,8 6.96875,11.71875 5,18 10,14 15,18 13.03125,11.71875 18,8 11.875,8 10,2 z" fill="#447eff" stroke="#447eff" stroke-width="2" stroke-linejoin="round"></path>' +
     '</svg>'
+// prettier-ignore
 const importerBadgeSvg =
     '<svg width="20" height="20">' +
     '<path d="M 10,2 8.125,8 2,8 6.96875,11.71875 5,18 10,14 15,18 13.03125,11.71875 18,8 11.875,8 10,2 z" fill="#38e13a" stroke="#38e13a" stroke-width="2" stroke-linejoin="round"></path>' +
