32e0dba70c1ef49344ba18b401fbfdb322fbe10f
Roman Deev <roman.deev06@gmail.com>
2025-12-14 03:07:57 +0300

    easter egg on osm.org error page

--- better-osm-org.user.js
+++ better-osm-org.user.js
@@ -22499,6 +22499,16 @@ function setupPrometheusLink() {
 <a href="https://github.com/openstreetmap/operations/issues?q=sort%3Aupdated-desc+is%3Aissue+is%3Aopen">OpenStreetMap operations issues</a>
 `
     document.body.appendChild(hint)
+    setTimeout(() => {
+        const iframe = GM_addElement("iframe", {
+            src: "https://deevroman.github.io/Dino-Game-Clone",
+            width: "70%",
+            height: "250px",
+            title: "better-osm-org easter egg :-)",
+        })
+        iframe.style.border = "0"
+        document.body.appendChild(iframe)
+    }, 4000)
 }
 
 //</editor-fold>
