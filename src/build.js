52e3d27c3056b03a6828e486043a30deff1b2d9e
Roman Deev <roman.deev06@gmail.com>
2026-03-14 12:40:22 +0300

    docs

--- src/build.js
+++ src/build.js
@@ -1,5 +1,10 @@
 import fs from "fs"
 import path from "path"
+import http from "http"
+import { execSync } from "child_process"
+
+const outPath = path.resolve("../better-osm-org.user.js")
+const args = new Set(process.argv.slice(2))
 
 function readJsFilesRecursively(dir) {
     const entries = fs.readdirSync(dir, { withFileTypes: true })
@@ -17,14 +22,49 @@ function readJsFilesRecursively(dir) {
     return result
 }
 
-setInterval(() => {
+function buildOnce() {
     const sources = readJsFilesRecursively(".").map(file => fs.readFileSync(file, "utf8"))
 
-    const outPath = "../better-osm-org.user.js"
-    const oldFile = fs.readFileSync(outPath, "utf8")
+    const oldFile = fs.existsSync(outPath) ? fs.readFileSync(outPath, "utf8") : ""
     const newFile = sources.join("\n")
     if (oldFile !== newFile) {
         fs.writeFileSync(outPath, newFile)
         console.log(new Date())
     }
-}, 500)
+}
+
+const shouldWatch = args.has("--watch")
+const shouldServe = args.has("--serve")
+
+buildOnce()
+if (shouldWatch) {
+    setInterval(buildOnce, 500)
+}
+
+const port = Number(process.env.PORT) || 7777
+const host = process.env.HOST || "127.0.0.1"
+
+if (shouldServe) {
+    const server = http.createServer((req, res) => {
+        try {
+            const url = new URL(req.url || "/", `http://${host}:${port}`)
+
+            let out
+            if (url.searchParams.has("master_user_js")) {
+                out = execSync("git show master:better-osm-org.user.js", { encoding: "utf8" })
+            } else {
+                out = fs.readFileSync(outPath, "utf8")
+            }
+
+            res.writeHead(200, { "content-type": "text/javascript; charset=utf-8" })
+            res.end(out)
+        } catch (error) {
+            res.writeHead(500, { "content-type": "text/plain; charset=utf-8" })
+            res.end(`Server error: ${error.message}`)
+        }
+    })
+
+    server.listen(port, host, () => {
+        console.log(`http://${host}:${port}/server.php?head.user.js`)
+    })
+}
