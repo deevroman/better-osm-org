a372b78af63481d48b3825d9e9075f7b3de879fa
Roman Deev <roman.deev06@gmail.com>
2026-03-14 13:13:47 +0300

    extract diff algorithm

---
+++ src/4-modules/42-diff-algorithm.js
@@ -0,0 +1,96 @@
+//<editor-fold desc="diff-algorithm" defaultstate="collapsed">
+
+/**
+ * Get editorial prescription via modified Levenshtein distance finding algorithm
+ * @template T
+ * @param {T[]} arg_a
+ * @param {T[]} arg_b
+ * @param {number} one_replace_cost
+ * @return {[T, T][]}
+ */
+function arraysDiff(arg_a, arg_b, one_replace_cost = 2) {
+    let a = arg_a.map(i => JSON.stringify(i))
+    let b = arg_b.map(i => JSON.stringify(i))
+    const dp = []
+    for (let i = 0; i < a.length + 1; i++) {
+        dp[i] = new Uint32Array(b.length + 1)
+    }
+
+    for (let i = 0; i <= a.length; i++) {
+        dp[i][0] = i
+    }
+
+    for (let i = 0; i <= b.length; i++) {
+        dp[0][i] = i
+    }
+
+    const min = Math.min // fuck Tampermonkey
+    // for some fucking reason every math.min call goes through TM wrapper code
+    // that is not optimized by the JIT compiler
+    if (arg_a.length && Object.prototype.hasOwnProperty.call(arg_a[0], "role")) {
+        for (let i = 1; i <= a.length; i++) {
+            for (let j = 1; j <= b.length; j++) {
+                const del_cost = dp[i - 1][j]
+                const ins_cost = dp[i][j - 1]
+                const replace_cost = dp[i - 1][j - 1] + (a[i - 1] !== b[j - 1]) * one_replace_cost // replacement is not very desirable
+                const replace_role_cost = dp[i - 1][j - 1] + (!(arg_a[i - 1].type === arg_b[j - 1].type && arg_a[i - 1].ref === arg_b[j - 1].ref) || arg_a[i - 1].role === arg_b[j - 1].role) * one_replace_cost
+                dp[i][j] = min(min(del_cost, ins_cost) + 1, min(replace_cost, replace_role_cost))
+            }
+        }
+    } else {
+        for (let i = 1; i <= a.length; i++) {
+            for (let j = 1; j <= b.length; j++) {
+                const del_cost = dp[i - 1][j]
+                const ins_cost = dp[i][j - 1]
+                const replace_cost = dp[i - 1][j - 1] + (a[i - 1] !== b[j - 1]) * one_replace_cost // replacement is not very desirable
+                dp[i][j] = min(min(del_cost, ins_cost) + 1, replace_cost)
+            }
+        }
+    }
+
+    a = a.map(i => JSON.parse(i))
+    b = b.map(i => JSON.parse(i))
+
+    const answer = []
+    let i = a.length
+    let j = b.length
+
+    while (true) {
+        if (i === 0 || j === 0) {
+            if (i === 0 && j === 0) {
+                break
+            } else if (i === 0) {
+                answer.push([null, b[j - 1]])
+                j = j - 1
+                continue
+            } else {
+                answer.push([a[i - 1], null])
+                i = i - 1
+                continue
+            }
+        }
+
+        const del_cost = dp[i - 1][j]
+        const ins_cost = dp[i][j - 1]
+        let replace_cost = dp[i - 1][j - 1] + (JSON.stringify(a[i - 1]) !== JSON.stringify(b[j - 1])) * one_replace_cost
+        if (arg_a.length && Object.prototype.hasOwnProperty.call(arg_a[0], "role")) {
+            replace_cost = min(replace_cost, dp[i - 1][j - 1] + (!(arg_a[i - 1].type === arg_b[j - 1].type && arg_a[i - 1].ref === arg_b[j - 1].ref) || arg_a[i - 1].role === arg_b[j - 1].role) * one_replace_cost)
+        }
+
+        if (del_cost <= ins_cost && del_cost + 1 <= replace_cost) {
+            answer.push([a[i - 1], null])
+            i = i - 1
+        } else if (ins_cost <= del_cost && ins_cost + 1 <= replace_cost) {
+            answer.push([null, b[j - 1]])
+            j = j - 1
+        } else {
+            answer.push([a[i - 1], b[j - 1]])
+            i = i - 1
+            j = j - 1
+        }
+    }
+    return answer.toReversed()
+}
+
+
+//</editor-fold>
