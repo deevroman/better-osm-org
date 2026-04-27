fd57099c33cd2f316d8e3bfd41feb7a813787dc0
Roman Deev <roman.deev06@gmail.com>
2026-04-28 01:58:46 +0300

    refactor

--- better-osm-org.user.js
+++ better-osm-org.user.js
@@ -25005,148 +25005,150 @@ out geom;
 
 //<editor-fold desc="drag-and-drop" defaultstate="collapsed">
 
-async function setupDragAndDropViewers() {
-    document.querySelector("#map")?.addEventListener("drop", e => {
-        if (location.pathname.includes("/directions") || location.pathname.includes("/note/new")) {
-            return
-        }
-        e.preventDefault()
-        e.stopPropagation()
-        e.stopImmediatePropagation()
-        e.target.style.cursor = "progress"
-        try {
-            const mapWidth = getComputedStyle(document.querySelector("#map")).width
-            const mapHeight = getComputedStyle(document.querySelector("#map")).height
-            insertOverlaysStyles()
-            ;[...e.dataTransfer.items].forEach(async (item, _) => {
-                if (item.kind === "file") {
-                    const file = item.getAsFile()
-                    if (file.type.startsWith("image/jpeg")) {
-                        const metadata = EXIF.readFromBinaryFile(await file.arrayBuffer())
-                        console.log(metadata)
-                        console.log(metadata.GPSLatitude, metadata.GPSLongitude)
-                        let lat =
-                            parseFloat(metadata.GPSLatitude[0]) +
-                            parseFloat(metadata.GPSLatitude[1]) / 60 +
-                            parseFloat(metadata.GPSLatitude[2]) / 3600
-                        let lon =
-                            parseFloat(metadata.GPSLongitude[0]) +
-                            parseFloat(metadata.GPSLongitude[1]) / 60 +
-                            parseFloat(metadata.GPSLongitude[2]) / 3600
+function dropEventHandler(e) {
+    if (location.pathname.includes("/directions") || location.pathname.includes("/note/new")) {
+        return
+    }
+    e.preventDefault()
+    e.stopPropagation()
+    e.stopImmediatePropagation()
+    e.target.style.cursor = "progress"
+    try {
+        const mapWidth = getComputedStyle(document.querySelector("#map")).width
+        const mapHeight = getComputedStyle(document.querySelector("#map")).height
+        insertOverlaysStyles()
+        ;[...e.dataTransfer.items].forEach(async (item, _) => {
+            if (item.kind === "file") {
+                const file = item.getAsFile()
+                if (file.type.startsWith("image/jpeg")) {
+                    const metadata = EXIF.readFromBinaryFile(await file.arrayBuffer())
+                    console.log(metadata)
+                    console.log(metadata.GPSLatitude, metadata.GPSLongitude)
+                    let lat =
+                        parseFloat(metadata.GPSLatitude[0]) +
+                        parseFloat(metadata.GPSLatitude[1]) / 60 +
+                        parseFloat(metadata.GPSLatitude[2]) / 3600
+                    let lon =
+                        parseFloat(metadata.GPSLongitude[0]) +
+                        parseFloat(metadata.GPSLongitude[1]) / 60 +
+                        parseFloat(metadata.GPSLongitude[2]) / 3600
 
-                        if (metadata.GPSLatitudeRef === "S") {
-                            lat = parseFloat(lat) * -1
-                        }
-
-                        if (metadata.GPSLongitudeRef === "W") {
-                            lon = parseFloat(lon) * -1
-                        }
-
-                        const marker = getWindow().L.circleMarker(
-                            getWindow().L.latLng(lat, lon),
-                            // intoPage({
-                            //     maxWidth: mapWidth,
-                            //     maxHeight: mapHeight,
-                            //     className: "map-img-preview-popup",
-                            // }),
-                        )
-                        const img = document.createElement("img")
-                        img.classList.add("geotagged-img")
-                        img.setAttribute("width", "100%")
-                        const fr = new FileReader()
-                        fr.onload = function () {
-                            img.src = fr.result
-                            marker.bindPopup(img.outerHTML)
-                        }
-                        fr.readAsDataURL(file)
-                        marker.addTo(getMap())
-                    } else if (file.type === "application/json" || file.type === "application/geo+json") {
-                        const geojson = JSON.parse(await file.text())
-                        renderGeoJSONwrapper(geojson)
-                    } else if (file.type === "application/gpx+xml") {
-                        displayGPXTrack(await file.text())
-                    } else if (file.type === "application/vnd.openstreetmap.data+xml") {
-                        const doc = new DOMParser().parseFromString(await file.text(), "application/xml")
-                        loadBannedVersions()
-                        preloadEditIcons()
-                        jsonLayer = renderOSMGeoJSON(doc, true)
-                    } else if (file.type === "application/vnd.google-earth.kml+xml") {
-                        displayKMLTrack(await file.text())
-                    } else if (file.type === "application/vnd.google-earth.kmz+xml") {
-                        const { entries } = await unzipit.unzip(await file.arrayBuffer())
-                        displayKMLTrack(
-                            await Object.entries(entries)
-                                .find(i => i[0].endsWith(".kml"))[1]
-                                .text(),
-                        )
-                    } else if (file.type === "text/csv") {
-                        const [header, ...lines] = (await file.text()).split("\n")
-                        let sep = header.includes(";") ? ";" : ","
-                        const columns = header
-                            .trim()
-                            .split(sep)
-                            .map(i => i.replace(/^"/g, "").replace(/"$/, ""))
-                        let latColumIndex = -1
-                        let lonColumIndex = -1
-                        columns.forEach((col, i) => {
-                            if (col.match(/(lat|широта)/i)) {
-                                latColumIndex = i
-                            } else if (col.match(/(lon|догота)/i)) {
-                                lonColumIndex = i
-                            }
-                        })
-                        if (latColumIndex === -1 || lonColumIndex === -1) {
-                            throw "Can't find lat/lon columns in CSV file"
-                        }
-
-                        /** @type {import("geojson").GeoJSON} */
-                        const geojson = {
-                            type: "FeatureCollection",
-                            features: [],
-                        }
-                        lines
-                            .map(line =>
-                                line
-                                    .trim()
-                                    .split(sep)
-                                    .map(i => {
-                                        if (i[0] === '"') {
-                                            if (i.at(-1) === '"') {
-                                                return i.slice(1, -1)
-                                            }
-                                            return i.slice(1)
-                                        }
-                                        if (i.at(-1) === '"') {
-                                            return i.slice(0, -1)
-                                        }
-                                        return i
-                                    }),
-                            )
-                            .forEach(values => {
-                                const lat = parseFloat(values[latColumIndex])
-                                const lon = parseFloat(values[lonColumIndex])
-                                if (isNaN(lat) || isNaN(lon)) {
-                                    console.warn("Invalid lat/lon in CSV file:", values)
-                                }
-                                geojson.features.push({
-                                    type: "Feature",
-                                    geometry: {
-                                        type: "Point",
-                                        coordinates: [lon, lat],
-                                    },
-                                    properties: Object.fromEntries(columns.map((col, i) => [col, values[i]])),
-                                })
-                            })
-                        renderGeoJSONwrapper(geojson)
-                    } else {
-                        console.log(file.type)
+                    if (metadata.GPSLatitudeRef === "S") {
+                        lat = parseFloat(lat) * -1
                     }
+
+                    if (metadata.GPSLongitudeRef === "W") {
+                        lon = parseFloat(lon) * -1
+                    }
+
+                    const marker = getWindow().L.circleMarker(
+                        getWindow().L.latLng(lat, lon),
+                        // intoPage({
+                        //     maxWidth: mapWidth,
+                        //     maxHeight: mapHeight,
+                        //     className: "map-img-preview-popup",
+                        // }),
+                    )
+                    const img = document.createElement("img")
+                    img.classList.add("geotagged-img")
+                    img.setAttribute("width", "100%")
+                    const fr = new FileReader()
+                    fr.onload = function () {
+                        img.src = fr.result
+                        marker.bindPopup(img.outerHTML)
+                    }
+                    fr.readAsDataURL(file)
+                    marker.addTo(getMap())
+                } else if (file.type === "application/json" || file.type === "application/geo+json") {
+                    const geojson = JSON.parse(await file.text())
+                    renderGeoJSONwrapper(geojson)
+                } else if (file.type === "application/gpx+xml") {
+                    displayGPXTrack(await file.text())
+                } else if (file.type === "application/vnd.openstreetmap.data+xml") {
+                    const doc = new DOMParser().parseFromString(await file.text(), "application/xml")
+                    loadBannedVersions()
+                    preloadEditIcons()
+                    jsonLayer = renderOSMGeoJSON(doc, true)
+                } else if (file.type === "application/vnd.google-earth.kml+xml") {
+                    displayKMLTrack(await file.text())
+                } else if (file.type === "application/vnd.google-earth.kmz+xml") {
+                    const { entries } = await unzipit.unzip(await file.arrayBuffer())
+                    displayKMLTrack(
+                        await Object.entries(entries)
+                            .find(i => i[0].endsWith(".kml"))[1]
+                            .text(),
+                    )
+                } else if (file.type === "text/csv") {
+                    const [header, ...lines] = (await file.text()).split("\n")
+                    let sep = header.includes(";") ? ";" : ","
+                    const columns = header
+                        .trim()
+                        .split(sep)
+                        .map(i => i.replace(/^"/g, "").replace(/"$/, ""))
+                    let latColumIndex = -1
+                    let lonColumIndex = -1
+                    columns.forEach((col, i) => {
+                        if (col.match(/(lat|широта)/i)) {
+                            latColumIndex = i
+                        } else if (col.match(/(lon|догота)/i)) {
+                            lonColumIndex = i
+                        }
+                    })
+                    if (latColumIndex === -1 || lonColumIndex === -1) {
+                        throw "Can't find lat/lon columns in CSV file"
+                    }
+
+                    /** @type {import("geojson").GeoJSON} */
+                    const geojson = {
+                        type: "FeatureCollection",
+                        features: [],
+                    }
+                    lines
+                        .map(line =>
+                            line
+                                .trim()
+                                .split(sep)
+                                .map(i => {
+                                    if (i[0] === '"') {
+                                        if (i.at(-1) === '"') {
+                                            return i.slice(1, -1)
+                                        }
+                                        return i.slice(1)
+                                    }
+                                    if (i.at(-1) === '"') {
+                                        return i.slice(0, -1)
+                                    }
+                                    return i
+                                }),
+                        )
+                        .forEach(values => {
+                            const lat = parseFloat(values[latColumIndex])
+                            const lon = parseFloat(values[lonColumIndex])
+                            if (isNaN(lat) || isNaN(lon)) {
+                                console.warn("Invalid lat/lon in CSV file:", values)
+                            }
+                            geojson.features.push({
+                                type: "Feature",
+                                geometry: {
+                                    type: "Point",
+                                    coordinates: [lon, lat],
+                                },
+                                properties: Object.fromEntries(columns.map((col, i) => [col, values[i]])),
+                            })
+                        })
+                    renderGeoJSONwrapper(geojson)
+                } else {
+                    console.log(file.type)
                 }
-            })
-        } finally {
-            e.target.style.cursor = "grab"
-        }
-    })
+            }
+        })
+    } finally {
+        e.target.style.cursor = "grab"
+    }
+}
+
+async function setupDragAndDropViewers() {
+    document.querySelector("#map")?.addEventListener("drop", dropEventHandler)
     document.querySelector("#map")?.addEventListener("dragover", e => {
         if (!location.pathname.includes("/directions") && !location.pathname.includes("/note/new")) {
             e.preventDefault()
