e9395f2859126960cef6fdc72a7902bcace58461
Roman Deev <roman.deev06@gmail.com>
2026-05-01 00:04:49 +0300

    refactor

--- src/0-common/18-editor-utils.js
+++ src/0-common/18-editor-utils.js
@@ -14,20 +14,6 @@ function tagsToXml(doc, node, tags) {
     }
 }
 
-function initOsmAuth() {
-    if (osmEditAuth === null) {
-        osmEditAuth = osmAuth.osmAuth({
-            apiUrl: osm_server.apiUrl,
-            url: osm_server.url,
-            client_id: "FwA",
-            client_secret: "ZUq",
-            redirect_uri: GM_getResourceURL("OAUTH_HTML"),
-            scope: "write_api",
-            auto: true,
-        })
-    }
-}
-
 /**
  *
  * @param {string} text
