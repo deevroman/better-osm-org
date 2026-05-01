649d2db0bd593c25d2a4eab8f9a53eff9a417425
Roman Deev <roman.deev06@gmail.com>
2026-05-02 02:30:57 +0300

    workaround for mobile file picker

--- src/4-modules/75-actions.js
+++ src/4-modules/75-actions.js
@@ -2427,6 +2427,7 @@ function actionOpenDevScriptUpdateUrl() {
 function actionOpenLocalFilePicker() {
     const input = document.createElement("input")
     input.type = "file"
+    input.accept = ".jpg,.jpeg,.csv,.kml,.kmz,.gpx,.json,.geojson"
     input.multiple = true
     input.addEventListener("change", () => {
         void handleDroppedFiles(Array.from(input.files ?? []))
