3f13481d1f568e1dd886c7d3101dcaeb8189f377
Roman Deev <roman.deev06@gmail.com>
2026-05-02 13:09:30 +0300

    split actions

--- src/4-modules/75-actions.js
+++ src/4-modules/75-actions.js
@@ -1162,14 +1162,14 @@ function actionToggleSwitchableTime() {
     document.querySelector("time[switchable]")?.click()
 }
 
-function actionHandleKeyT() {
-    if (location.pathname.includes("/user/") && !location.pathname.includes("/history")) {
-        document.querySelector('a[href="/traces/mine"], a[href$="/traces"]:not(.nav-link):not(.dropdown-item)')?.click()
-    } else {
-        document.querySelector(".quick-look-compact-toggle-btn")?.click()
-        document.querySelector(".compact-toggle-btn")?.click()
-        actionToggleSwitchableTime()
-    }
+function actionOpenUserTraces() {
+    document.querySelector('a[href="/traces/mine"], a[href$="/traces"]:not(.nav-link):not(.dropdown-item)')?.click()
+}
+
+function actionToggleCompactMode() {
+    document.querySelector(".quick-look-compact-toggle-btn")?.click()
+    document.querySelector(".compact-toggle-btn")?.click()
+    actionToggleSwitchableTime()
 }
 
 function actionOpenOverpassSearch() {
