64c929cec51797de1ab56d2c45aa8f576c89cf7c
Roman Deev <roman.deev06@gmail.com>
2026-05-11 01:14:23 +0300

    add warn about big CSV

--- src/4-modules/74-drag-and-drop.js
+++ src/4-modules/74-drag-and-drop.js
@@ -3,6 +3,9 @@
 function displayCsv(text) {
     const [header, ...lines] = text.split("\n")
     console.log("lines count", lines.length)
+    if (lines.length > 20000) {
+        alert("Too many lines in CSV file. Only object in bbox will be displayed")
+    }
     let sep = header.includes(";") ? ";" : ","
     const columns = header
         .trim()
