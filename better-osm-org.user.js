f04bfcb494c1624ef6e4b9b4403d4463f7053b5f
Roman Deev <roman.deev06@gmail.com>
2026-07-06 13:40:55 +0300

    respect ctrl + click on map

--- better-osm-org.user.js
+++ better-osm-org.user.js
@@ -9530,6 +9530,10 @@ async function getElementsAroundNode(lat, lng) {
     throw "empty region"
 }
 
+/**
+ * @param {PointerEvent} e
+ * @return {Promise<void>}
+ */
 async function mapClickHandler(e) {
     if (skipClick) {
         return
@@ -9656,7 +9660,16 @@ async function mapClickHandler(e) {
     if (location.pathname.startsWith("/changeset") /* && !confirm(`Open ${targetURL}`)*/) {
         return
     }
-    getWindow().OSM.router.route(targetURL)
+    if (e.ctrlKey || e.metaKey) {
+        window.open(location.origin + targetURL)
+    } else {
+        try {
+            getWindow().OSM.router.route(targetURL)
+        } catch (e) {
+            window.open(location.origin + targetURL)
+        }
+    }
+
     console.log(elements)
 }
 
