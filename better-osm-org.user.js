1637b0c3a12ca816ce1b39a473b49af8d45cd2cf
Roman Deev <roman.deev06@gmail.com>
2026-09-04 03:56:23 +0300

    better settings fields

--- better-osm-org.user.js
+++ better-osm-org.user.js
@@ -5530,7 +5530,6 @@ const configOptions = {
                 min-height: 21px;
             }
             #Config [placeholder]:empty::before {
-                white-space: pre;
                 content: attr(placeholder);
                 color: #555;
             }
