c732875f02d1b3985a6cb706ca1d0d6b0dc33c8c
Roman Deev <roman.deev06@gmail.com>
2026-09-06 16:01:34 +0300

    don't add resolve button for closed notes

--- src/4-modules/80-id-editor.js
+++ src/4-modules/80-id-editor.js
@@ -108,6 +108,9 @@ function addResolveNotesButtonInId() {
     if (document.querySelector(".notes-buttons-wrapper")) {
         return true
     }
+    if (document.querySelector(".note-header-icon.closed")) {
+        return true
+    }
 
     const buttonsWrapper = document.createElement("span")
     buttonsWrapper.classList.add("notes-buttons-wrapper")
