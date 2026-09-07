bae2695de83748d532524a55223b58f92a2898aa
Roman Deev <roman.deev06@gmail.com>
2026-09-07 04:18:04 +0300

    Vespucci export draft

--- src/6-not-osm-org/87-overpass-turbo.js
+++ src/6-not-osm-org/87-overpass-turbo.js
@@ -63,6 +63,36 @@ function addLevel0Reborn() {
     }
     l0reborn.after(l0rebornBbox)
     l0reborn.after(document.createTextNode("\xA0"))
+    /*
+    const vespucci = l0export.cloneNode(true)
+    vespucci.id = "export-editors-vespucci"
+    vespucci.textContent = "Vespucci"
+    vespucci.setAttribute("href", "")
+    vespucci.onclick = function () {
+        function extractIds() {
+            const text = document.querySelector("#dataviewer .CodeMirror-lines").textContent
+            try {
+                const json = JSON.parse(text)
+                return {
+                    nodes: json.elements.filter(i => i.type === "node").map(i => i.id),
+                    ways: json.elements.filter(i => i.type === "way").map(i => i.id),
+                    relations: json.elements.filter(i => i.type === "relation").map(i => i.id),
+                }
+            } catch (e) {
+                const xml = new DOMParser().parseFromString(text, "text/xml")
+                return {
+                    nodes: Array.from(xml.querySelectorAll("node")).map(i => i.getAttribute("id")),
+                    ways: Array.from(xml.querySelectorAll("way")).map(i => i.getAttribute("id")),
+                    relations: Array.from(xml.querySelectorAll("relation")).map(i => i.getAttribute("id")),
+                }
+            }
+        }
+        const { nodes, ways, relations } = extractIds()
+        vespucci.setAttribute("href", makeVespucciMultiUrl(nodes, ways, relations))
+    }
+    l0rebornBbox.after(vespucci)
+    l0rebornBbox.after(document.createTextNode("\xA0"))
+*/
 }
 
 function setupOverpass() {
