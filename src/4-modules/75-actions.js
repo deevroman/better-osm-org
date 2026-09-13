701ca85e8053aa33fa65daf8e17980dca57e7a56
Roman Deev <roman.deev06@gmail.com>
2026-09-13 19:52:08 +0300

    hardening: drop innerHTML = "<svg>"

--- src/4-modules/75-actions.js
+++ src/4-modules/75-actions.js
@@ -1684,10 +1684,7 @@ async function showHotkeyCommandsPopup() {
     closeBtn.classList.add("better-btn-close", "better-osm-hotkey-commands-close")
     closeBtn.type = "button"
     closeBtn.setAttribute("aria-label", "Close commands popup")
-    closeBtn.innerHTML =
-        '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">' +
-        '  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>' +
-        "</svg>"
+    insertSvg(closeBtn, "close")
     closeBtn.querySelector("svg").style.height = "1.25rem"
     closeBtn.onclick = closeHotkeyCommandsPopup
 
