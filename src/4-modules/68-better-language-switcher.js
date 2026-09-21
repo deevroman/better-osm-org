b9b4ebf04d78b5fe668533eab0bd94af965f3387
Roman Deev <roman.deev06@gmail.com>
2026-09-21 03:34:48 +0300

    fix crash

--- src/4-modules/68-better-language-switcher.js
+++ src/4-modules/68-better-language-switcher.js
@@ -1,18 +1,20 @@
 //<editor-fold desc="" defaultstate="collapsed">
 
 function setupLanguageSwitcher() {
-    const langSwitch = document.querySelector("#header-nav .bi-translate").parentElement
-    if (!langSwitch || langSwitch.classList.contains("better-lang-switch")) {
-        return
-    }
-    langSwitch.classList.add("better-lang-switch")
-    langSwitch.addEventListener("click", e => {
-        if (e.ctrlKey || e.metaKey) {
-            setTimeout(() => {
-                document.querySelector('[href*="locale=en"]').click()
-            }, 1000)
+    setTimeout(() => {
+        const langSwitch = document.querySelector("#header-nav .bi-translate")?.parentElement
+        if (!langSwitch || langSwitch.classList.contains("better-lang-switch")) {
+            return
         }
-    })
+        langSwitch.classList.add("better-lang-switch")
+        langSwitch.addEventListener("click", e => {
+            if (e.ctrlKey || e.metaKey) {
+                setTimeout(() => {
+                    document.querySelector('[href*="locale=en"]').click()
+                }, 1000)
+            }
+        })
+    }, 1000)
 }
 
 //</editor-fold>
